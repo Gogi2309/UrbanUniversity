978064f071ca2edc99786907755bd4a18321cbd2
Gogi2309 <drandrov921@yandex.ru>
2024-09-01 16:27:29 +0300

    Add files via upload

---
+++ Базовые структуры данных ПЗ.py
@@ -0,0 +1,11 @@
+# "1st program"
+print(((9)**0.5)*5)
+# "2nd programm"
+print(9.99>=9.98 and 1000!=10001)
+# "3rd programm"
+print(2*2+2)
+print ((2*(2+2)))
+print( (2*2+2)==(2*(2+2 )) )
+# "4th programm"
+print(float('123.456')*10)
+print('123.456'[4])
