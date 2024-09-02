5e80b57fce002141de754d80da5c6e3eae5d8cba
Gogi2309 <drandrov921@yandex.ru>
2024-09-02 20:14:26 +0300

    Add files via upload

---
+++ ПЗ Переменные.py
@@ -0,0 +1,5 @@
+home_work_complited = 12
+elapsed_time = 1.5
+course_name = 'Python'
+time_for_one_task = 0.125
+print('Курс:',course_name,',всего задач:',home_work_complited,',затрачено часов:',elapsed_time,',среднее время выполнения',time_for_one_task,'часа' , sep='')
