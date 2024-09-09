e5c9fb29309fd60c80e46b299e6a1fd45085f16f
Gogi2309 <drandrov921@yandex.ru>
2024-09-09 14:34:58 +0300

    Add files via upload

---
+++ module_1_5_1.py
@@ -0,0 +1,8 @@
+immutable_var=1,2,3,'apple','coconat'
+print(immutable_var)
+mutable_list=[1,2,3,'apple','coconat']
+print(mutable_list)
+mutable_list[2]=33
+print(mutable_list)
+mutable_list[3]='banana'
+print(mutable_list)
