accd04631c0a6e11f29be91347eb75524f2b5863
Gogi2309 <drandrov921@yandex.ru>
2024-09-09 21:38:54 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict={'Milana': 2012, 'Radmila': 2013, 'Kamila': 2021}
+print(my_dict)
+print(my_dict['Milana'])
+print(my_dict.get('Papa'))
+my_dict.update({'Mama':1980,'Deda':1950})
+print(my_dict)
+a = my_dict.pop('Deda')
+print(my_dict)
+print(a)
+my_set = {5,6,7,8,5,6,False,(4,5,6)}
+print(my_set)
+print(my_set.add('a'))
+print(my_set.add('True'))
+print(my_set)
+print(my_set.remove(5))
+print(my_set)
